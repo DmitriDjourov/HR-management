4eaec6eba86b3bd8addbdf2fd56083572b64b524
DmitriDjourov <135020293+DmitriDjourov@users.noreply.github.com>
2023-09-24 00:37:08 +0300

    Update ServiceCollection.java
    added spaces

--- HR-management/src/main/java/org/example/ServiceCollection.java
+++ HR-management/src/main/java/org/example/ServiceCollection.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 
 public class ServiceCollection {
-		// - показать всех отсортировав по имени
+// - показать всех отсортировав по имени
 		public void sortAllEmployeesName(List<Employee> employees) {
 				Collections.sort(employees);
 		}
 
-		// - показать всех отсортировав по зарплате
+// - показать всех отсортировав по зарплате
 		public void sortAllEmployeesSalary(List<Employee> employees) {
 				employees.sort(Comparator.comparingDouble(Employee::getSalary));
 		}
