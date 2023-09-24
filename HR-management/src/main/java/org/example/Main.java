9fcff72ae4c86b68ae5c34b929177eeac3852388
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2023-09-24 19:42:40 +0300

    added serviceCollection.getAvgSalDep

--- HR-management/src/main/java/org/example/Main.java
+++ HR-management/src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static org.example.ServiceCollection.getAvgSalDep;
 
 public class Main {
 		public static void main(String[] args) {
@@ -20,7 +23,7 @@ public class Main {
 
 // - показать всех отсортировав по имени
 				System.out.println(" - показать всех отсортировав по имени");
-				serviceCollection.getSortAllEmployeesName(employees);
+				ServiceCollection.getSortAllEmployeesName(employees);
 
 				for (Employee employee : employees) {
 						System.out.println(employee.toString());
@@ -29,7 +32,7 @@ public class Main {
 
 // - показать всех отсортировав по зарплате
 				System.out.println(" - показать всех отсортировав по зарплате");
-				serviceCollection.getSortAllEmployeesSalary(employees);
+				ServiceCollection.getSortAllEmployeesSalary(employees);
 
 				for (Employee employee : employees) {
 						System.out.println(employee.toString());
@@ -38,12 +41,21 @@ public class Main {
 
 // - показать имя, фамилию, департамент и грейд
 				System.out.println(" - показать имя, фамилию, департамент и грейд");
-				List<String> employee4Fields = serviceCollection.getDisplNameSurnameDeptGrade(employees);
+				List<String> employee4Fields = ServiceCollection.getDisplNameSurnameDeptGrade(employees);
 
 				for (String employee4F : employee4Fields) {
 						System.out.println(employee4F);
 				}
 				System.out.println();
+
+// - показать среднюю зарплату по департаментам
+				System.out.println(" - показать среднюю зарплату по департаментам");
+				Map<Department, Double> depAvgSal= serviceCollection.getAvgSalDep(employees);
+
+				for (Department department : depAvgSal.keySet()) {
+						double avgSal = depAvgSal.get(department);
+						System.out.printf("Департамент: %s, Средняя зарплата: %.2f%n", department, avgSal);
+				}
 		}
 }
 
--- HR-management/src/main/java/org/example/ServiceCollection.java
+++ HR-management/src/main/java/org/example/ServiceCollection.java
@@ -1,24 +1,21 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 
 public class ServiceCollection {
 		// - показать всех отсортировав по имени
-		public void getSortAllEmployeesName(List<Employee> employees) {
+		public static void getSortAllEmployeesName(List<Employee> employees) {
 				Collections.sort(employees);
 		}
 
 		// - показать всех отсортировав по зарплате
-		public void getSortAllEmployeesSalary(List<Employee> employees) {
+		public static void getSortAllEmployeesSalary(List<Employee> employees) {
 				employees.sort(Comparator.comparingDouble(Employee::getSalary));
 		}
 
 		// - показать имя, фамилию, департамент и грейд
-		public List<String> getDisplNameSurnameDeptGrade(List<Employee> employees) {
+		public static List<String> getDisplNameSurnameDeptGrade(List<Employee> employees) {
 				List<String> fourFildsInfoList = new ArrayList<>();
 				for (Employee employee : employees) {
 						String fourFieldsInfo = "  " + employee.getName() + "  " + employee.getSurname() + "  "
@@ -26,6 +23,30 @@ public class ServiceCollection {
 						fourFildsInfoList.add(fourFieldsInfo);
 				}
 				return fourFildsInfoList;
+		}
+
+		// - показать среднюю зарплату по департаментам
+		public static Map<Department, Double> getAvgSalDep(List<Employee> employees) {
+				Map<Department, Double> depAvgSal = new HashMap<>();
+				Map<Department, Integer> depEmpCount = new HashMap<>();
+
+				for (Employee employee : employees) {
+						Department department = employee.getDepartment();
+						double salary = employee.getSalary();
+
+						depAvgSal.put(department, depAvgSal.getOrDefault(department, 0.0) + salary);
+						depEmpCount.put(department, depEmpCount.getOrDefault(department, 0) + 1);
+				}
+
+				for (Department department : depAvgSal.keySet()) {
+						double totalSal = depAvgSal.get(department);
+						int empCount = depEmpCount.get(department);
+						double avgSal = totalSal / empCount;
+
+						depAvgSal.put(department, avgSal);
+				}
+
+				return depAvgSal;
 		}
 }
 
